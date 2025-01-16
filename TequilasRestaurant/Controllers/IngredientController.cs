a586988accb04318545662dd0be576faf2cb8375
robertgruevski <robertgruevski@outlook.com>
2025-01-16 17:01:04 -0500

    Create ingredient implemented

--- TequilasRestaurant/Controllers/IngredientController.cs
+++ TequilasRestaurant/Controllers/IngredientController.cs
@@ -20,5 +20,24 @@ namespace TequilasRestaurant.Controllers
 		{
 			return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
 		}
+
+		// Ingredient/Create
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("IngredientId, Name")] Ingredient ingredient)
+		{
+			if (ModelState.IsValid)
+			{
+				await ingredients.AddAsync(ingredient);
+				return RedirectToAction("Index");
+			}
+			return View(ingredient);
+		}
 	}
 }
