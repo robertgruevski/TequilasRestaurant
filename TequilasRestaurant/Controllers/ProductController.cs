4ad9f2ef2d47904011a9c3d341fa95a91e2343ce
robertgruevski <robertgruevski@outlook.com>
2025-01-16 17:44:33 -0500

    Product index route

---
+++ TequilasRestaurant/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TequilasRestaurant.Data;
+using TequilasRestaurant.Models;
+
+namespace TequilasRestaurant.Controllers
+{
+	public class ProductController : Controller
+	{
+		private Repository<Product> products;
+		public ProductController(ApplicationDbContext context)
+		{
+			this.products = new Repository<Product>(context);
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View(await products.GetAllAsync());
+		}
+	}
+}
