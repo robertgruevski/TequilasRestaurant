dd4a4dcd6d3dbb7efa8328b36bf001342db46942
robertgruevski <robertgruevski@outlook.com>
2025-01-16 15:22:07 -0500

    Model creation

--- TequilasRestaurant/Models/Order.cs
+++ TequilasRestaurant/Models/Order.cs
@@ -4,5 +4,9 @@
 	{
 		public int OrderId { get; set; }
 		public DateTime OrderDate { get; set; }
+		public string? UserId { get; set; }
+		public ApplicationUser User { get; set; }
+		public decimal TotalAmount { get; set; }
+		public ICollection<OrderItem> OrderItems { get; set; }
 	}
 }
